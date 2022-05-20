9d6fcab87d1bae1389ec56d0d6ba909fcc43409f
pennyluc <pennyluc321@gmail.com>
2022-05-20 12:59:41 +0100

    add function added'

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ function add(a, b) {
     return a + b
 }
 
+const add2 = (a, b) => {
+return a + b;
+}
 
-
+const num2 = add2(2,2)
+console.log(num2)
 const number = add(1,2)
 console.log(number)
