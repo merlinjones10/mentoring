3861480c04a5e55eb25fa4e0d532042dbc7d3c2f
merlinjones10 <merlinjones10@gmail.com>
2022-05-20 12:49:51 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function hello1() {
+    console.log("Hello")
+}
+
+const hello2 = () => {
+    console.log("hello");
+}
+
+const name = 'merlin'
+
+function add(a, b) {
+    return a + b
+}
+
+
+
+const number = add(1,2)
+console.log(number)
